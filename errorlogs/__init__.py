451782ac66c48bd4d85e75a714fb214d295a3b7d
Alex <58824393+vertyco@users.noreply.github.com>
2022-08-12 00:18:19 -0400

    Update __init__.py

--- errorlogs/__init__.py
+++ errorlogs/__init__.py
@@ -6,5 +6,5 @@ from .errorlogs import ErrorLogs
 __red_end_user_data_statement__ = "This cog does not store end user data."
 
 
-def setup(bot: Red):
+async def setup(bot: Red):
     bot.add_cog(ErrorLogs())
